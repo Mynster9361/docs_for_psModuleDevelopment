fa1edf33efe592bc963c1c94f84dac957ac6e14a
-Mynster <66535357+Mynster9361@users.noreply.github.com>
2026-01-19 22:42:00 +0100

    fix: Update GitHub API endpoint and improve request formatting in release script

--- build/vsts-release.ps1
+++ build/vsts-release.ps1
@@ -32,23 +32,23 @@ Compress-Archive -Path "$WorkingDirectory\publish\YourModuleName\*" -Destination
 
 # Step 2: Create Release
 Write-Host "Registering new release for version $($moduleVersion) with Github"
-$response = Invoke-RestMethod -Method POST -Uri 'https://api.github.com/repos/YourGithubUserName/YourModuleName/releases' -Headers @{
-	Authorization = "Bearer $env:GH_TOKEN"
-	Accept = 'application/vnd.github+json'
+$response = Invoke-RestMethod -Method POST -Uri 'https://api.github.com/repos/Mynster9361/docs_for_psModuleDevelopment/releases' -Headers @{
+	Authorization          = "Bearer $env:GH_TOKEN"
+	Accept                 = 'application/vnd.github+json'
 	'X-GitHub-Api-Version' = '2022-11-28'
 } -Body (@{
-	tag_name = "v$moduleVersion"
-	name = "v$moduleVersion"
-	body = "Releasing v$moduleVersion of the YourModuleName module."
-	make_latest = 'true'
-} | ConvertTo-Json -Depth 10 -Compress)
+		tag_name    = "v$moduleVersion"
+		name        = "v$moduleVersion"
+		body        = "Releasing v$moduleVersion of the YourModuleName module."
+		make_latest = 'true'
+	} | ConvertTo-Json -Depth 10 -Compress)
 
 # Step 3: Upload ZIP as Release content
 
 Write-Host "Publishing module archive to new release"
 Invoke-RestMethod -Method POST -Uri "$($response.assets_url -replace 'api\.github\.com', 'uploads.github.com')?name=YourModuleName.zip" -Headers @{
-	Authorization = "Bearer $env:GH_TOKEN"
-	Accept = 'application/vnd.github+json'
+	Authorization          = "Bearer $env:GH_TOKEN"
+	Accept                 = 'application/vnd.github+json'
 	'X-GitHub-Api-Version' = '2022-11-28'
-	'Content-Type' = 'application/octet-stream'
+	'Content-Type'         = 'application/octet-stream'
 } -InFile "$WorkingDirectory\publish\YourModuleName.zip"
